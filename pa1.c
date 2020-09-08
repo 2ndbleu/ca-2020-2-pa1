8f4219c5ce4d6f8098237d248c9c8036eacee070
Injae Kang <abcinje@gmail.com>
2020-09-08 14:01:03 +0000

    Add skeleton code

---
+++ pa1.c
@@ -0,0 +1,21 @@
+//---------------------------------------------------------------
+//
+//  4190.308 Computer Architecture (Fall 2020)
+//
+//  Project #1: Compressing Data with Huffman Coding
+//
+//  September 9, 2020
+//
+//  Injae Kang (abcinje@snu.ac.kr)
+//  Sunmin Jeong (sunnyday0208@snu.ac.kr)
+//  Systems Software & Architecture Laboratory
+//  Dept. of Computer Science and Engineering
+//  Seoul National University
+//
+//---------------------------------------------------------------
+
+/* TODO: Implement this function */
+int encode(const char *inp, int inbytes, char *outp, int outbytes)
+{
+	return 0;
+}
